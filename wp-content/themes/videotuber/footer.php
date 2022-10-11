8b0ac0ec55f5313cfbbd8483e7a90fa131b10207
xSeraphim <aldeardaniel@gmail.com>
2022-10-12 00:39:28 +0300

    implemented footer and theme mod for footer background color

--- wp-content/themes/videotuber/footer.php
+++ wp-content/themes/videotuber/footer.php
@@ -1,3 +1,48 @@
 <?php wp_footer(); ?>
+
+<div id="footer" class="footer">
+	<div class="videotuber-socials">
+		<h3>Follow me on:</h3>
+		<a class="videotuber-social-link" href="https://twitter.com/YouTube" target="_blank" rel="noreferrer nofollow" aria-label="Twitter" title="Twitter">
+		<?php echo '<img src='. WPR_VIDEOTUBER_PATH . '/assets/icons/facebook.svg>' ?>
+		</a>
+		<a class="videotuber-social-link" href="https://twitter.com/YouTube" target="_blank" rel="noreferrer nofollow" aria-label="Twitter" title="Twitter">
+		<?php echo '<img src='. WPR_VIDEOTUBER_PATH . '/assets/icons/instagram.svg>' ?>
+		</a>
+		<a class="videotuber-social-link" href="https://twitter.com/YouTube" target="_blank" rel="noreferrer nofollow" aria-label="Twitter" title="Twitter">
+		<?php echo '<img src='. WPR_VIDEOTUBER_PATH . '/assets/icons/tiktok.svg>' ?>
+		</a>
+		<a class="videotuber-social-link" href="https://twitter.com/YouTube" target="_blank" rel="noreferrer nofollow" aria-label="Twitter" title="Twitter">
+		<?php echo '<img src='. WPR_VIDEOTUBER_PATH . '/assets/icons/twitter.svg>' ?>
+		</a>
+		<a class="videotuber-social-link" href="https://twitter.com/YouTube" target="_blank" rel="noreferrer nofollow" aria-label="Twitter" title="Twitter">
+		<?php echo '<img src='. WPR_VIDEOTUBER_PATH . '/assets/icons/youtube.svg>' ?>
+		</a>
+	</div>
+	<div class="videotuber-middle-footer">
+	<?php
+	wp_nav_menu(array(
+		'theme_location' => 'foooterMiddleLocation1',
+	  ));
+	  wp_nav_menu(array(
+		'theme_location' => 'foooterMiddleLocation2',
+	  ));
+	  wp_nav_menu(array(
+		'theme_location' => 'foooterMiddleLocation3',
+	  ));
+	?>
+	</div>
+	<div class="videotuber-bottom-footer">
+	<?php if ( function_exists( 'the_custom_logo' ) ) { 
+				the_custom_logo();
+				}
+				wp_nav_menu(array(
+					'theme_location' => 'foooterBottomLocation',
+				  ));
+	?>
+
+	</div>
+	   
+	</div>
 	</body>
 </html>
--- wp-content/themes/videotuber/functions.php
+++ wp-content/themes/videotuber/functions.php
@@ -21,8 +21,13 @@ add_action( 'wp_enqueue_scripts', 'videtuber_add_styles' );
 
 function videotuber_features() {
 	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'custom-logo' );
 	register_nav_menu( 'headerMenuLocation', 'Header Main Menu Navigation' );
+	register_nav_menu( 'foooterMiddleLocation1', 'Footer Middle Menu Navigation 1' );
+	register_nav_menu( 'foooterMiddleLocation2', 'Footer Middle Menu Navigation 2' );
+	register_nav_menu( 'foooterMiddleLocation3', 'Footer Middle Menu Navigation 3' );
+	register_nav_menu( 'foooterBottomLocation', 'Bottom Footer Menu Navigation' );
 }
 add_action( 'after_setup_theme', 'videotuber_features' );
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'custom-logo' );
+
